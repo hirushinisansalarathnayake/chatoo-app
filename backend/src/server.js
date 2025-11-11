8582efa0162ff0dfa2a6fd9a7712fddbbf1befb6
Hirushi Rathnayake <hirushi.rathnayake@dmselectronics.com>
2025-11-11 15:07:11 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,16 +2,29 @@ import dotenv from 'dotenv';
 import express from 'express';
 import authRoutes from './routes/auth.routes.js';
 import messageRoutes from './routes/message.routes.js';
+import path from 'path';
 
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
 
 
 const PORT = process.env.PORT || 3000;
 
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
+
+
+//make app for production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+ app.get('*', (_, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+});
+
+}
 
 console.log(process.env.PORT);
 
